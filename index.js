dfb734ad7171b11baeb3b5f77a10ff15c2cfeb1d
DaniK <danikordonezgarcia@gmail.com>
2023-01-11 20:38:21 +0100

    fix client.color and added intents to Client

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const { Client, GatewayIntentBits } = require('discord.js');
 const Discord = require('discord.js');
 const config = require('./config/config.json');
 require('colors');
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildVoiceStates,
-	],
-});
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.GuildEmojisAndStickers,
+    ],
+    partials: [Discord.Partials.User, Discord.Partials.Channel, Discord.Partials.GuildMember, Discord.Partials.Message, Discord.Partials.Reaction]
+
+})
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+client.color = config.color;
 
 function requireHandlers() {
     ['commands', 'events', "distube"].forEach(handler => {
