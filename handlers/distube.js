b5dd04d6ea9a9109b5b70c15a8716198080f63df
DaniK <danikordonezgarcia@gmail.com>
2023-01-11 20:37:38 +0100

    addSong and initQueue

--- handlers/distube.js
+++ handlers/distube.js
@@ -2,14 +2,15 @@ const {DisTube} = require('distube');
 // const {SpotifyPlugin} = require('@distube/spotify');
 // const {SoundCloudPlugin} = require('@distube/soundcloud');
 
-module.exports = (client) => {
+module.exports = (client, Discord) => {
     client.distube = new DisTube(client, {
         emitNewSongOnly: false,
         leaveOnEmpty: true,
         leaveOnFinish: false,
         leaveOnStop: true,
+        savePreviousSongs: true,
         emitAddListWhenCreatingQueue: false,
-        searchSongs: 3,
+        searchSongs: 0,
         nsfw: false,
         emptyCooldown: 30,
         ytdlOptions: {
@@ -27,5 +28,33 @@ module.exports = (client) => {
         //     }),
         //     new SoundCloudPlugin()
         // ]
+    });
+
+    client.distube.on("playSong", (queue, song) => {
+        queue.textChannel.send({
+            embeds: [new Discord.EmbedBuilder()
+            .setTitle(`Reproduciendo \`${song.name}\` - \`${song.formattedDuration}\``)
+            .setThumbnail(song.thumbnail)
+            .setURL(song.url)
+            .setColor(client.color)
+            .setFooter({text: `Añadida por ${song.user.tag}`, iconURL: song.user.displayAvatarURL({dynamic: true})})
+            ]
+        })
+    })
+
+    client.distube.on("addSong", (queue, song) => {
+        queue.textChannel.send({
+            embeds: [new Discord.EmbedBuilder()
+            .setTitle(`✅ Añadido \`${song.name}\` - \`${song.formattedDuration}\``)
+            .setThumbnail(song.thumbnail)
+            .setURL(song.url)
+            .setColor(client.color)
+            .setFooter({text: `Añadida por ${song.user.tag}`, iconURL: song.user.displayAvatarURL({dynamic: true})})
+            ]
+        })
+    });
+
+    client.distube.on("initQueue", (queue) => {
+        queue.autoplay = true;
     });
 }
